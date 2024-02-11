85a8dcedbf675a45aef84d880a27015e075d5c1f
arushisinha98 <115908316+arushisinha98@users.noreply.github.com>
2024-02-11 23:17:31 +0800

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -22,27 +22,6 @@ credit_categories = [
     'Other'
 ]
 
-account_names = {
-    'DBS' : 'SGD',
-    'DBS - FD' : 'SGD',
-    'DBS - SRS' : 'SGD',
-    'CPF' : 'SGD',
-    'IBKR' : 'SGD',
-    'Endowus' : 'SGD',
-    'CDP' : 'SGD',
-    'Instarem' : 'SGD',
-    'PayLah!' : 'SGD',
-    'Bank of America' : 'USD',
-    'Robinhood - Cash' : 'USD',
-    'Robinhood - Personal' : 'USD',
-    'Robinhood - Shared' : 'USD',
-    'Robinhood - Roth IRA' : 'USD',
-    'Fidelity - Roth IRA' : 'USD',
-    'Fidelity - IRA' : 'USD',
-    'Treasury - iBonds' : 'USD',
-    'E*TRADE - ESPP' : 'USD'
-}
-
 converter = {
     'USD': 1,
     'SGD': 0.74

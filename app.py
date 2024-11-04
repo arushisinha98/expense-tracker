129dc55da6e562a39cb68b4b2ec2a3e3402d533e
arushisinha98 <arushisinha98+github@gmail.com>
2024-11-04 20:04:43 +0000

    fix: raise useful errors

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 import os
 
+from src.constants import tabs
+
 from src.main import MAIN
 from src.help import HELP
 
 
 def main():
-    if not os.path.isdir('data') or not os.path.isdir('data/misc'):
+    # check data directory path exists
+    if not os.path.isdir('data'):
         HELP()
+        raise MissingDirectoryError("Require a `data/` folder in root.")
+    # check each tab has an associated currency
+    if not all(['currency' in list(value.keys()) for key, value in tabs.items()]):
+        HELP()
+        raise MissingCurrencyError("Require each tab to have an associated currency.")
+    # check each currency is accounted for in converter
+    if not all(tab['currency'] in list(converter.keys()) for tab in tabs.values()):
+        HELP()
+        raise MissingConversionError("We do not perform real-time currency conversions. Please declare all currency conversions in `constants.py`.")
+    # check data directory is organized by tab
+    if not all([os.path.exists(f"data/{tab['tag']}/") for tab in tabs.values()]):
+        HELP()
+        raise MismatchDirectoryError("Require each tab to have an associated subdirectory in `data/`.")
     try:
         MAIN()
     except Exception as e:
